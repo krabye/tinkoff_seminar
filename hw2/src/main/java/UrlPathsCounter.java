d37c4f24768dc4b2d09d3772c835dda9aebcb1b1
Vladislav Efimov <v.efimov@corp.mail.ru>
2018-10-23 02:07:06 +0300

    try fix

--- hw2/src/main/java/UrlPathsCounter.java
+++ hw2/src/main/java/UrlPathsCounter.java
@@ -73,10 +73,10 @@ public class UrlPathsCounter extends Configured implements Tool {
 
         @Override
         protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
-            String[] split = value.toString().split("\t");
-            String url = split[2];
-
-            context.write(new Text(url), NullWritable.get());
+//            String[] split = value.toString().split("\t");
+//            String url = split[2];
+//
+//            context.write(new Text(url), NullWritable.get());
         }
     }
 
